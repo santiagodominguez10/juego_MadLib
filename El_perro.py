5770faccfa1efbf6d201d9a8ad201ff57070e67d
santiagodominguez10 <sdominguezpagani@gmail.com>
2023-05-12 07:38:40 -0300

    El perro en el parque

---
+++ El_perro.py
@@ -0,0 +1,9 @@
+print("Bienvenido al juego Mad Libs! Por favor, ingrese las siguientes palabras para completar la historia:")
+
+verbo_acción = input("Ingrese un verbo de acción: ")
+adverbio_como_corria = input("Ingrese un adverbio de como corria: ")
+adjetivo_descripción = input("Ingrese un adjetivo de descripción: ")
+
+print(f'El perro {verbo_acción}.')
+print(f'{adverbio_como_corria} por el parque.')
+print(f'Era muy {adjetivo_descripción} y divertido de ver.')
